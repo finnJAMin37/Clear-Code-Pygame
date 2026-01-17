82f98e8fbb44fe6faeaf78f413c61da41c1c7863
Finn Leckrone <finnleckrone@gmail.com>
2026-01-16 22:22:44 -0500

    day 1 file testing

---
+++ 5games/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+# general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption("test")
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+    display_surface.fill('lightcyan1')
+    pygame.display.update()
+
+pygame.quit()
