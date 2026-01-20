3a9df7160145e233137b68ef8c8e4f87dbd8d572
Finn Leckrone <finnleckrone@gmail.com>
2026-01-19 19:14:27 -0500

    Day 4: Vectors, delta time, pygame clocks

--- 5games/space shooter/main.py
+++ 5games/space shooter/main.py
@@ -8,6 +8,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
+clock = pygame.time.Clock()
 
 # meteor
 meteor_path = join("images","meteor.png")
@@ -24,13 +25,15 @@ star_path = join("images", "star.png")
 star_surf = pygame.image.load(star_path).convert_alpha()
 star_positions = [(randint(0,WINDOW_WIDTH),randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
-# importing image
+# player importing image
 player_path = join("images","player.png")
 player_surf = pygame.image.load(player_path).convert_alpha()
 player_rect = player_surf.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_dirx = 100
+player_dir = pygame.math.Vector2(-1, -1)
+player_speed = 400
 
 while running:
+    dt = clock.tick(60) / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,15 +47,14 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
 
     # player movement
-    if player_dirx > 0:
-        if player_rect.right < WINDOW_WIDTH:
-            player_rect.centerx += player_dirx
-        else: 
-            player_dirx = abs(player_dirx) * -1
-    elif player_rect.left > 0:
-        player_rect.centerx += player_dirx
-    else:
-        player_dirx = abs(player_dirx)
+    if player_rect.top < 0 or player_rect.bottom > WINDOW_HEIGHT:
+        player_dir[1] *= -1
+        if player_rect.left < 0 or player_rect.right > WINDOW_WIDTH:
+            pygame.display.set_caption("CORNER!!")
+            player_dir[0] *= -1
+    elif player_rect.left < 0 or player_rect.right > WINDOW_WIDTH:
+        player_dir[0] *= -1
+    player_rect.center += player_dir * player_speed * dt
 
     display_surface.blit(player_surf, player_rect)
     
