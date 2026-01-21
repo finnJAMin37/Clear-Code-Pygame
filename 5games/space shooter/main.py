5571974dc68676fe42d835272bca35c0d39e8b89
Finn Leckrone <finnleckrone@gmail.com>
2026-01-20 21:37:34 -0500

    Day 5!: closing in on 1 week!, keyboard and mouse input today, busy but still got it done!

--- 5games/space shooter/main.py
+++ 5games/space shooter/main.py
@@ -29,7 +29,7 @@ star_positions = [(randint(0,WINDOW_WIDTH),randint(0, WINDOW_HEIGHT)) for i in r
 player_path = join("images","player.png")
 player_surf = pygame.image.load(player_path).convert_alpha()
 player_rect = player_surf.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_dir = pygame.math.Vector2(-1, -1)
+player_dir = pygame.math.Vector2(0, 0)
 player_speed = 400
 
 while running:
@@ -38,6 +38,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    #    if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+    #        print(1)
+    #    if event.type == pygame.MOUSEMOTION:
+    #        player_rect.center = event.pos
+    # print(pygame.mouse.get_pressed())
+    
+    keys = pygame.key.get_just_pressed()
+    player_dir.x = int(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT])
+    player_dir.y = int(keys[pygame.K_DOWN] - keys[pygame.K_UP])
+    if int(keys[pygame.K_SPACE]):
+        print("fire laser!") 
+
 
     # draw the game
     display_surface.fill('darkgrey')
@@ -46,17 +58,13 @@ while running:
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
 
-    # player movement
-    if player_rect.top < 0 or player_rect.bottom > WINDOW_HEIGHT:
-        player_dir[1] *= -1
-        if player_rect.left < 0 or player_rect.right > WINDOW_WIDTH:
-            pygame.display.set_caption("CORNER!!")
-            player_dir[0] *= -1
-    elif player_rect.left < 0 or player_rect.right > WINDOW_WIDTH:
-        player_dir[0] *= -1
+    #
+    player_dir = player_dir.normalize() if player_dir else player_dir
     player_rect.center += player_dir * player_speed * dt
 
     display_surface.blit(player_surf, player_rect)
+
+    print(player_dir.magnitude())
     
     pygame.display.update()
 
