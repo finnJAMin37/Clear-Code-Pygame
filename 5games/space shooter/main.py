ca1e53d6b845b984e5bbe17305063cb15c15c7aa
Finn Leckrone <finnleckrone@gmail.com>
2026-01-26 19:37:39 -0500

    Day 7: collisions: done with basic loop for meteor game!

--- 5games/space shooter/main.py
+++ 5games/space shooter/main.py
@@ -26,7 +26,7 @@ class Player(pygame.sprite.Sprite):
         self.dir.y = int(keys[pygame.K_DOWN] - keys[pygame.K_UP])
         keys = pygame.key.get_just_pressed()
         if int(keys[pygame.K_SPACE]) and self.can_shoot:
-            Laser(laser_surf, self.rect.midtop, all_sprites)
+            Laser(laser_surf, self.rect.midtop, (all_sprites, laser_sprites))
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
         self.dir = self.dir.normalize() if self.dir else self.dir
@@ -66,7 +66,17 @@ class Meteor(pygame.sprite.Sprite):
         if self.rect.top > WINDOW_HEIGHT:
              self.kill()
 
+def collisions():
+    global running
 
+    collided_sprites = pygame.sprite.spritecollide(player, meteor_sprites, True)
+    if collided_sprites:
+        print("player hit", collided_sprites[0])
+        running = False
+    for laser in laser_sprites:
+        collided_sprites = pygame.sprite.spritecollide(laser, meteor_sprites, True)
+        if collided_sprites:
+            laser.kill()
 
 # general setup
 pygame.init()
@@ -83,6 +93,8 @@ star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
 
 # sprites
 all_sprites = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
 for i in range(20):
     Star(all_sprites, star_surf)
 player = Player(all_sprites)
@@ -98,13 +110,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == meteor_event:
-            Meteor(meteor_surf, all_sprites)
+            Meteor(meteor_surf, (all_sprites, meteor_sprites))
 
     all_sprites.update(dt)
+    # collisions
+    collisions()
 
     # draw the game
     display_surface.fill('darkgrey')
-
     all_sprites.draw(display_surface)
 
     pygame.display.update()
